625e9c81b0974b21316618dd1262e3c6a011e304
duyuan <2698663919@qq.com>
2024-07-12 23:01:20 +0800

    initial commit

---
+++ elmclient/src/components/Error403.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <img src="../assets/error403.png"><br>
+    <router-link to="/index">服务器繁忙中，返回重试</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error403"
+}
+</script>
+
+<style scoped>
+  .wrapper{
+    text-align: center;
+  }
+  .wrapper img{
+    width: 100%;
+    margin: 120px 0 20px;
+  }
+  .wrapper a{
+    color: #0097EF;
+  }
+</style>
